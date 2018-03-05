bb20470d5c407f1ae5107df31f0c1faf64f39275
hungvv <hungvv@macbook-pro-cua-mac.local>
2018-03-05 11:41:41 +0700

    Initial Commit

--- StoreSearch/controller/SearchViewController.swift
+++ StoreSearch/controller/SearchViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 
 class SearchViewController : UIViewController {
-
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-     
+        searchBar.delegate = self
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +23,14 @@ class SearchViewController : UIViewController {
         // Dispose of any resources that can be recreated.
         
     }
+    
+ 
 
 
+}
+
+extension SearchViewController : UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("Text Search : '\(searchBar.text!)'")
+    }
 }
