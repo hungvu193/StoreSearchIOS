7ca6eea45a5b6c5311e6bf8d728ecd549535b87c
hungvv <hungvv@MacBook-Pro-cua-Mac.local>
2018-03-05 16:51:54 +0700

    Add custom cell

---
+++ StoreSearch/xib/SearchResultCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by hungvv on 3/5/18.
+//  Copyright © 2018 hungvv. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
