b01a2ada79d27357d4986282aab0073f37352765
hungvv <hungvv@macbook-pro-cua-mac.local>
2018-03-05 15:39:03 +0700

    added search bar

--- StoreSearch/controller/SearchViewController.swift
+++ StoreSearch/controller/SearchViewController.swift
@@ -11,11 +11,17 @@ import UIKit
 class SearchViewController : UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+    var searchResults : [SearchResult] = []
+    
+    var hasSearched = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        searchBar.delegate = self
+   
         tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+        let cellNib = UINib(nibName: "SearchResultCell", bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: "SearchResultCell")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,8 +35,71 @@ class SearchViewController : UIViewController {
 
 }
 
-extension SearchViewController : UISearchBarDelegate {
+extension SearchViewController : UITableViewDelegate{
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        print("Text Search : '\(searchBar.text!)'")
+        searchResults = []
+        searchBar.resignFirstResponder()
+        if searchBar.text != "th"{
+            for i in 0...2{
+                let searchResult = SearchResult()
+                searchResult.name = String(format: "Fake Result %d for", i)
+                searchResult.artistName = searchBar.text!
+                searchResults.append(searchResult)
+            }
+        }
+        
+        hasSearched = true
+        tableView.reloadData()
+    }
+}
+
+extension SearchViewController : UISearchBarDelegate{
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+}
+
+extension SearchViewController : UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if !hasSearched{
+            return 0
+        }
+        else
+            if searchResults.count == 0 {
+                return 1
+            }else{
+                return searchResults.count
+        }
+    }
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if searchResults.count == 0{
+            //khi ko có kết quả trả về return nil để row đó ko bấm vào đc
+            return nil
+        }else{
+            return indexPath
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchResultCell"
+        var cell : UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style : .subtitle , reuseIdentifier : cellIdentifier)
+        }
+       
+        if searchResults.count == 0 {
+            cell.textLabel!.text = "(Nothing found)"
+            cell.detailTextLabel!.text = ""
+        } else {
+            let searchResult = searchResults[indexPath.row]
+            cell.textLabel!.text = searchResult.name
+            cell.detailTextLabel!.text = searchResult.artistName
+        }
+        return cell
     }
 }
---
+++ StoreSearch/model/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by hungvv on 3/5/18.
+//  Copyright © 2018 hungvv. All rights reserved.
+//
+
+import Foundation
+class SearchResult{
+    var name = ""
+    var artistName = ""
+}
